74af7be6048ae4bce1d9cbbe93afb9daef58d153
彭凯 <tyut_pengkai@163.com>
2022-02-20 22:46:43 +0800

    对家长端的帮助中心、系统通知等进行更新

--- pages/system_notion/system_notion.js
+++ pages/system_notion/system_notion.js
@@ -20,52 +20,10 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 点击消息，查看消息详情
+    check_message: function () {
+        wx.navigateTo({
+          url: '../message_content/message_content',
+        })
     }
 })
