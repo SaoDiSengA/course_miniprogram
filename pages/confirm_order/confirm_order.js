79442eeb560d1a4390038c163e713e7c8d71471c
彭凯 <tyut_pengkai@163.com>
2022-02-21 20:57:42 +0800

    测试用户支付接口

--- pages/confirm_order/confirm_order.js
+++ pages/confirm_order/confirm_order.js
@@ -1,17 +1,69 @@
 // pages/confirm_order/confirm_order.js
+
+// 获取APP的全局变量值
+var app = getApp()
+console.log(app.globalData)
+var util = require('../../utils/util.js')
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+      open_id: null,
+      course_info: null
+    },
 
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      // 获取课程详情
+      var course_info = app.globalData.course_data
+      // 转换对象作用域
+      var that = this
+      // 传递数据
+      that.setData({
+        course_info: course_info
+      })
+      // 调用云函数获取用户的openid
+      wx.cloud.callFunction({
+        name: 'get_openid',
+        success(res){
+          that.setData({
+            open_id: res.result.openid
+          })
+        }
+      })
     },
 
     
+    // 用户点击支付
     pay_click: function () {
-        wx.navigateTo({
-          url: '../order_success/order_success',
-        })
+      // 获取课程详情
+      var course_info = this.data.course_info
+      // 获取openId
+      var open_id = this.data.open_id
+      wx.request({
+        url: 'http://localhost:8080/WXPay/goWeChatPay',
+        method: 'POST',
+        header: {
+          'content-type': "application/x-www-form-urlencoded",
+        },
+        data: {
+          'openid': open_id,
+          'detail': course_info.title,
+        },
+        success(res){
+          console.log(res.data)
+          // wx.requestPayment({
+          //   nonceStr: 'nonceStr',
+          //   package: 'package',
+          //   paySign: 'paySign',
+          //   timeStamp: 'timeStamp',
+          // })
+        }
+      })
     }
 })
